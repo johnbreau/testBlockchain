233cfd40274deb99f09e5e5e46381a8cc540de6a
John Breau <john.breau@vml.com>
2018-01-12 09:58:54 -0600

    further work on blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,39 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactiions = []
 
-    def new_block(self):
-        #creates a new block and adds it to the chain
-        pass
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+        
+        block= {
+            'index' = len(self.chain) + 1,
+            'timestamp' = time(),
+            'transactions' = self.current_transactiions,
+            'proof' = proof,
+            'previous_hash' = previous_hash or self.hash(self.chain[-1]),
+            }
+
+        self.current_transactiions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recepient, amount):
         """
         Creates a new transaction to go into the next mined Block
-
         :param sender: <str> Address of the Sender
         :param recipient: <str> Address of the Recipient
         :param amount: <int> Amount
