8c63a9a9f3e2fe8e860926e205b4d4d05f69e0c2
John Breau <john.breau@vml.com>
2018-01-12 20:31:14 -0600

    adding flask

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+import textwrap import dedent
 from time import time
+from uuid import uuid4
 
 
 class Blockchain(object):
@@ -52,10 +54,65 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-        #hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         #returns the last block in the chain
         pass        
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is false:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-','')
+
+blockchain = new Blockchain()
+
+app.route('/mine', methods=['GET'])
+
+def mine():
+    return "We will mine a new block"
+
+app.route('/chain', methods = ['GET'])
+
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jasonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
